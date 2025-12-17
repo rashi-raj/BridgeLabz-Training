15b4054073ba5a34112f892c72999605e8621ef6
rashi0122 <rashiraj847@gmail.com>
2025-12-17 16:35:51 +0530

    [Rashi Raj] : Add centimeter conversion program

---
+++ gcr-codebase/programming-elements/Level-1-practice-problem/ConversionOfCentimeter.java
@@ -0,0 +1,19 @@
+// Import Scanner class to take input from the user
+import java.util.Scanner;
+
+// Create ConversionOfCentimeter class to convert centimeters into feet and inches
+ 
+public class ConversionOfCentimeter{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		double centimeters = input.nextDouble();
+		
+		//Convert height in centimeters to inches
+		double inch = 2.54 * centimeters;
+		
+		//Convert height in inch to foot
+		double foot = 12 * inch;
+		
+		System.out.print("Your height in cm is " + centimeters + " while in feet is " + foot + " and inches is " + inch);
+	}
+}

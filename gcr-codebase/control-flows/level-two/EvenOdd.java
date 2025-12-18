f4e025f959176670ebb3c42ec5c38df0ca94f93e
rashi0122 <rashiraj847@gmail.com>
2025-12-18 23:40:03 +0530

    [Rashi Raj] : Add even odd program

---
+++ gcr-codebase/control-flows/level-two/EvenOdd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// Create EvenOdd class to print odd numbers and even numbers between 1 to the number entered by the user
+public class EvenOdd{
+	public static void main(String args[]){
+		// Create a Scanner Object
+		Scanner input = new Scanner(System.in);
+		
+		// Get input value for number
+		int number = input.nextInt();
+		
+		// Check and print even and odd number
+		for(int i=1 ; i <= number ; i++){
+			if(i % 2 == 0){
+				System.out.println(i + " is even number!");
+			}
+			else{
+				System.out.println(i + " is odd number!");
+			}
+		}
+		
+		// Closing the Scanner Stream
+		input.close();
+	}
+}

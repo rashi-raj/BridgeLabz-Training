a8d0f86116872a4fecd297828025086ed0f48aa0
rashi0122 <rashiraj847@gmail.com>
2025-12-30 19:12:01 +0530

    [Rashi Raj] : Add Power program

---
+++ leet-code-codebase/Basic-problems/Power.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/powx-n/
+// Problem - 50
+
+public class Power{
+    public double myPow(double x, int n) {
+        double ans=1.0;
+        long nn = n;
+        if(nn<0) nn = -1*nn;
+        while(nn>0){
+            if(nn%2!=0){
+                ans = ans*x;
+                nn = nn-1;
+            }else{
+                x = x*x;
+                nn = nn/2;
+            }
+        }
+        if(n<0) ans = 1/ans;
+        return ans;
+    }
+}

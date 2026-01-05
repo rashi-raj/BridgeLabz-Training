78b5f1d8b77a52d3c7c7bd4bbad8fbcf08d3ae6e
rashi0122 <rashiraj847@gmail.com>
2026-01-05 23:28:58 +0530

    [Rashi Raj] : Updated Longest Consecutive program

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/hashmap/LongestConsecutive.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/hashmap/LongestConsecutive.java
@@ -1,5 +1,31 @@
 package com.hashmap;
 
-public class LongestConsecutive {
+import java.util.*;
 
+public class LongestConsecutive{
+    public static int longestSequence(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) set.add(num);
+
+        int longest = 0;
+
+        for (int num : arr) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest consecutive sequence length: " + longestSequence(arr));
+    }
 }

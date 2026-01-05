ad4c7b8513dafe825e11c5027e52abcc261f4252
rashi0122 <rashiraj847@gmail.com>
2026-01-05 23:30:18 +0530

    [Rashi Raj] : Updated Zero Sum subarrays program

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/hashmap/ZeroSumSubarrays.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/hashmap/ZeroSumSubarrays.java
@@ -1,5 +1,29 @@
 package com.hashmap;
 
-public class ZeroSumSubarrays {
+import java.util.*;
 
+public class ZeroSumSubarrays {
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+
+        map.put(0, new ArrayList<>(Arrays.asList(-1)));
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int startIndex : map.get(sum)) {
+                    System.out.println("Subarray found from index " + (startIndex + 1) + " to " + i);
+                }
+            }
+
+            map.computeIfAbsent(sum, k -> new ArrayList<>()).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 1, 3, -4, -2, -2};
+        findZeroSumSubarrays(arr);
+    }
 }

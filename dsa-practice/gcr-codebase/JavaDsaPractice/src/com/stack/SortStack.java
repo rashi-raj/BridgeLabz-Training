aa5c8e62d1e2c1f0f412057dab340ceb6a19cadf
rashi0122 <rashiraj847@gmail.com>
2026-01-05 23:46:06 +0530

    [Rashi Raj] : Updated Stack programs

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stack/SortStack.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stack/SortStack.java
@@ -1,5 +1,35 @@
 package com.stack;
 
-public class SortStack {
+import java.util.Stack;
 
+public class SortStack{
+
+    static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int top = stack.pop();
+            sortStack(stack);
+            insertSorted(stack, top);
+        }
+    }
+
+    static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
+            stack.push(element);
+            return;
+        }
+
+        int temp = stack.pop();
+        insertSorted(stack, element);
+        stack.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(30);
+        stack.push(10);
+        stack.push(20);
+
+        sortStack(stack);
+        System.out.println(stack);
+    }
 }
--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stack/StockSpan.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stack/StockSpan.java
@@ -1,5 +1,30 @@
 package com.stack;
 
+import java.util.Stack;
+import java.util.Arrays;
+
 public class StockSpan {
 
+    static int[] calculateSpan(int[] price) {
+        int n = price.length;
+        int[] span = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        span[0] = 1;
+        stack.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && price[stack.peek()] <= price[i]) {
+                stack.pop();
+            }
+            span[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        return span;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        System.out.println(Arrays.toString(calculateSpan(prices)));
+    }
 }

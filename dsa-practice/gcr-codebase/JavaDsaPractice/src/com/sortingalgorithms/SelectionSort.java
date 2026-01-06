13dbd1370a22592ca5a8e792620e45f577d5ff2c
rashi0122 <rashiraj847@gmail.com>
2026-01-06 12:08:45 +0530

    [Rashi Raj] : Updated SelectionSort program

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/SelectionSort.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/SelectionSort.java
@@ -1,5 +1,23 @@
 package com.sortingalgorithms;
 
-public class SelectionSort {
+import java.util.Arrays;
 
+public class SelectionSort{
+    public static void main(String[] args) {
+        int[] scores = {88, 72, 95, 64, 81};
+
+        for (int i = 0; i < scores.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < scores.length; j++) {
+                if (scores[j] < scores[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = scores[minIndex];
+            scores[minIndex] = scores[i];
+            scores[i] = temp;
+        }
+
+        System.out.println("Sorted Exam Scores: " + Arrays.toString(scores));
+    }
 }

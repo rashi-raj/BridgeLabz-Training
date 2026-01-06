443219edf70491b666abe92012543daf9cbfe9e2
rashi0122 <rashiraj847@gmail.com>
2026-01-06 11:40:17 +0530

    [Rashi Raj] : Updated program

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/InsertionSort.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/InsertionSort.java
@@ -1,5 +1,22 @@
 package com.sortingalgorithms;
 
-public class InsertionSort {
+import java.util.Arrays;
 
+public class InsertionSort{
+    public static void main(String[] args) {
+        int[] empIds = {105, 101, 109, 103, 102};
+
+        for (int i = 1; i < empIds.length; i++) {
+            int key = empIds[i];
+            int j = i - 1;
+
+            while (j >= 0 && empIds[j] > key) {
+                empIds[j + 1] = empIds[j];
+                j--;
+            }
+            empIds[j + 1] = key;
+        }
+
+        System.out.println("Sorted Employee IDs: " + Arrays.toString(empIds));
+    }
 }
---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/InsertionSortt.java
@@ -0,0 +1,5 @@
+package com.sortingalgorithms;
+
+public class InsertionSort {
+
+}

c85a2833c11bce4a34d7eb74b6bac20302632162
rashi0122 <rashiraj847@gmail.com>
2026-01-06 12:12:28 +0530

    [Rashi Raj] : Updated QuickSort program

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/QuickSort.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithms/QuickSort.java
@@ -1,5 +1,40 @@
 package com.sortingalgorithms;
 
-public class QuickSort {
+import java.util.Arrays;
 
+public class QuickSort{
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1200, 450, 800, 300, 650};
+        quickSort(prices, 0, prices.length - 1);
+        System.out.println("Sorted Product Prices: " + Arrays.toString(prices));
+    }
 }

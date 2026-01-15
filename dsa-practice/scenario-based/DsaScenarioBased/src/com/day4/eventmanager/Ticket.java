720ee8ec396f71452dc41f871ef5071f2e7d4612
rashi0122 <rashiraj847@gmail.com>
2026-01-15 23:04:31 +0530

    [Rashi Raj] : Add Event manager program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/com/day4/eventmanager/Ticket.java
@@ -0,0 +1,16 @@
+package com.day4.eventmanager;
+
+public class Ticket {
+    String eventName;
+    double price;
+
+    public Ticket(String eventName, double price) {
+        this.eventName = eventName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return eventName + " -> ₹" + price;
+    }
+}

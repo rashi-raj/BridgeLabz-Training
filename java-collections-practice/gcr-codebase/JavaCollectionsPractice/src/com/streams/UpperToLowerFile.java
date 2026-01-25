1483aa6b3cfd2c8aefcf9178d7eb08c673358173
rashi0122 <rashiraj847@gmail.com>
2026-01-25 23:47:06 +0530

    [Rashi Raj] : Add Upper to Lower file program

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/streams/UpperToLowerFile.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/streams/UpperToLowerFile.java
@@ -1,5 +1,25 @@
 package com.streams;
 
-public class UpperToLowerFile {
+/*
+6. Filter Streams - Convert Uppercase to Lowercase
+Read a text file and write lowercase content to another file.
+*/
+import java.io.*;
 
+public class UpperToLowerFile {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line.toLowerCase());
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

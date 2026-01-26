6591c95c494ac7a2648abdfacd149330a0ef50b2
rashi0122 <rashiraj847@gmail.com>
2026-01-26 23:12:23 +0530

    [Rashi Raj] : Add role based access control program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/rolebasedaccesscontrol/RoleAllowed.java
@@ -0,0 +1,9 @@
+package com.annotation.rolebasedaccesscontrol;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RoleAllowed {
+    String value();
+}

93b02961752c091367cb7dbf2f1528ce34ca1943
rashi0122 <rashiraj847@gmail.com>
2026-01-26 23:12:03 +0530

    [Rashi Raj] : Add repeatable annotation program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/repeatableannotation/BugReport.java
@@ -0,0 +1,10 @@
+package com.annotation.repeatableannotation;
+
+import java.lang.annotation.*;
+
+@Repeatable(BugReports.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BugReport {
+    String description();
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/repeatableannotation/BugReports.java
@@ -0,0 +1,9 @@
+package com.annotation.repeatableannotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BugReports {
+    BugReport[] value();
+}

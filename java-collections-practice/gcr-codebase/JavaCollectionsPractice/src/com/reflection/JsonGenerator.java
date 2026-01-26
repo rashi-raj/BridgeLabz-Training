3a8c89425c48fc144ab818fe27f2a10992b044b0
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:28:16 +0530

    [Rashi Raj] : Add Json generator program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/JsonGenerator.java
@@ -0,0 +1,21 @@
+package com.reflection;
+
+import java.lang.reflect.Field;
+
+public class JsonGenerator {
+
+    public static String toJson(Object obj) throws Exception {
+        StringBuilder sb = new StringBuilder("{");
+
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field f : fields) {
+            f.setAccessible(true);
+            sb.append("\"").append(f.getName()).append("\":\"")
+              .append(f.get(obj)).append("\",");
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("}");
+        return sb.toString();
+    }
+}

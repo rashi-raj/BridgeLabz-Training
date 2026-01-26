2d359be104db265cd87e50014b21b0eb20b85254
rashi0122 <rashiraj847@gmail.com>
2026-01-26 23:10:15 +0530

    [Rashi Raj] : Add Custom serialization program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/customserialization/JsonField.java
@@ -0,0 +1,9 @@
+package com.annotation.customserialization;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface JsonField {
+    String name();
+}

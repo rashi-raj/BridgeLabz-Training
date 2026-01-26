ab30edd6eb5d672971bece6c59e1e4f2a148ad9b
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:18:47 +0530

    [Rashi Raj] : Add String utils testing program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/jnuit/stringutils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.jnuit.stringutils;
+
+public class StringUtils {
+
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        return str.equals(reverse(str));
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}

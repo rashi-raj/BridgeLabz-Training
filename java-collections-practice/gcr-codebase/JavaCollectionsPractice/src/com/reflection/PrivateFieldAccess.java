346789f5232c8ef1ad383e84065cb8734ba6feeb
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:35:59 +0530

    [Rashi Raj] : Updated program

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/PrivateFieldAccess.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/PrivateFieldAccess.java
@@ -1,5 +1,21 @@
 package com.reflection;
 
-public class PrivateFieldAccess {
+import java.lang.reflect.Field;
 
+class Person {
+    private int age = 20;
+}
+
+
+public class PrivateFieldAccess {
+    public static void main(String[] args) throws Exception {
+
+        Person p = new Person();
+
+        Field field = Person.class.getDeclaredField("age");
+        field.setAccessible(true);
+
+        field.set(p, 30);
+        System.out.println("Age = " + field.get(p));
+    }
 }

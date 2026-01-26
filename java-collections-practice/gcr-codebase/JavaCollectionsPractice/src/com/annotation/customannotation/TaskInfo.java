3a29f6f7d00ef9c34869d41fef67fce1dfc1845f
rashi0122 <rashiraj847@gmail.com>
2026-01-26 23:09:58 +0530

    [Rashi Raj] : Add Custom annotation program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/customannotation/TaskInfo.java
@@ -0,0 +1,10 @@
+package com.annotation.customannotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TaskInfo {
+    String priority();
+    String assignedTo();
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotation/customannotation/TaskManager.java
@@ -0,0 +1,19 @@
+package com.annotation.customannotation;
+
+import java.lang.reflect.Method;
+
+public class TaskManager {
+
+    @TaskInfo(priority = "HIGH", assignedTo = "Rashi")
+    public void completeTask() {
+        System.out.println("Task completed");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Method m = TaskManager.class.getMethod("completeTask");
+        TaskInfo info = m.getAnnotation(TaskInfo.class);
+
+        System.out.println(info.priority());
+        System.out.println(info.assignedTo());
+    }
+}

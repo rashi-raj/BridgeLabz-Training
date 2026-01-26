d9bd933170ee90b0a4a02c99d1f477061a839632
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:17:16 +0530

    [Rashi Raj] : Add Exception testing program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/jnuit/excpetiontest/ExceptionTest.java
@@ -0,0 +1,14 @@
+package com.jnuit.excpetiontest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionTest {
+
+    @Test
+    void testArithmeticException() {
+        assertThrows(ArithmeticException.class, () -> {
+            int a = 10 / 0;
+        });
+    }
+}

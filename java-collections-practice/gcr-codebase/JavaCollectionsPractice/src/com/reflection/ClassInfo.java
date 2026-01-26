64eb5258bcb6cc384da6d7ecee99acfdd3c8449d
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:35:34 +0530

    [Rashi Raj] : Updated program

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ClassInfo.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ClassInfo.java
@@ -1,5 +1,28 @@
 package com.reflection;
 
-public class ClassInfo {
+import java.lang.reflect.*;
 
+public class ClassInfo {
+    public static void main(String[] args) throws Exception {
+
+        Class<?> cls = Class.forName("java.util.ArrayList");
+
+        System.out.println("Class Name: " + cls.getName());
+
+        System.out.println("\nFields:");
+        for (Field f : cls.getDeclaredFields()) {
+            System.out.println(f);
+        }
+
+        System.out.println("\nMethods:");
+        for (Method m : cls.getDeclaredMethods()) {
+            System.out.println(m);
+        }
+
+        System.out.println("\nConstructors:");
+        for (Constructor<?> c : cls.getDeclaredConstructors()) {
+            System.out.println(c);
+        }
+    }
+    
 }

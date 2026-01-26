0cc4e3e638c86ac87ead8b21f2eb6e7f2dd3d476
rashi0122 <rashiraj847@gmail.com>
2026-01-26 22:19:20 +0530

    [Rashi Raj] : Add Timeout testing program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/jnuit/timeout/TimeoutTest.java
@@ -0,0 +1,13 @@
+package com.jnuit.timeout;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class TimeoutTest {
+
+    @Test
+    @Timeout(2)
+    void testLongRunningTask() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+}

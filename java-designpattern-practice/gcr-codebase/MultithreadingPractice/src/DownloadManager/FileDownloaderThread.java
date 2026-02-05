7d51e827c1a045f13ecb59f22104967ef510f656
rashi0122 <rashiraj847@gmail.com>
2026-02-05 11:14:34 +0530

    [Rashi Raj] : Add Download manager program

---
+++ java-designpattern-practice/gcr-codebase/MultithreadingPractice/src/DownloadManager/FileDownloaderThread.java
@@ -0,0 +1,24 @@
+package DownloadManager;
+class FileDownloaderThread extends Thread {
+
+    private String fileName;
+
+    public FileDownloaderThread(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int progress = 0; progress <= 100; progress += 10) {
+                System.out.println(
+                    "[" + Thread.currentThread().getName() + "] Downloading "
+                    + fileName + ": " + progress + "%"
+                );
+                Thread.sleep((int)(Math.random() * 500)); // random delay
+            }
+        } catch (InterruptedException e) {
+            System.out.println(fileName + " download interrupted");
+        }
+    }
+}
